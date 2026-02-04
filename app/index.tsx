a0f6171b8a18bfe276e0e0d3658498e148fd341f
madiweber <madi.weber@informatik.hs-fulda.de>
2026-02-04 16:03:05 +0100

    added location storage

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { createAsyncStorage } from "@react-native-async-storage/async-storage";
 import * as Location from 'expo-location';
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
@@ -6,20 +7,48 @@ type weatherData = {
   temperature: number;
 }
 
+type permissionStatus = {
+  status: 'undetermined' | 'granted' | 'denied';
+}
+
 export default function Index() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [weather, setWeather] = useState<weatherData | null>(null);
+  const [permissionStatus, setPermissionStatus] = useState<permissionStatus | null>(null);
+
+  const storage = createAsyncStorage("appDB");
 
   async function getLocation() {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      //console.log('Permission to access location was denied');
-      return;
+    if (permissionStatus?.status == 'granted') {
+      const jsonValue = await storage.getItem("location");
+      if (jsonValue != null) {
+        const storedLocation: Location.LocationObject = JSON.parse(jsonValue);
+        setLocation(storedLocation);
+        //console.log('Location retrieved from AsyncStorage: ' + jsonValue);
+        return;
+      }
+      else { }
     }
     else {
-      let location = await Location.getCurrentPositionAsync({ accuracy: 6 });
-      setLocation(location);
-      //console.log(location);
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setPermissionStatus({ status: 'denied' });
+
+        let location = await Location.getCurrentPositionAsync({ accuracy: 6 });
+        setPermissionStatus({ status: 'granted' });
+        const storeData = async () => {
+          try {
+            const jsonValue = JSON.stringify(location);
+            await storage.setItem("location", jsonValue);
+          }
+          catch (e) {
+            console.log('Error saving location to AsyncStorage: ' + e);
+          }
+        }
+        storeData();
+        setLocation(location);
+        //console.log(location);
+      }
     }
   }
 
@@ -30,10 +59,13 @@ export default function Index() {
   }
 
   useEffect(() => {
-    console.log("useEffect");
-    getLocation();
-    getTemperatur();
-  }, [location?.coords.latitude, location?.coords.longitude]);
+    if (location == null) {
+      getLocation();
+    }
+    else {
+      getTemperatur();
+    }
+  }, [weather?.temperature]);
 
   return (
     <View
@@ -42,7 +74,7 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-    > 
+    >
       {/* <Text>{location ? `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}` : "Standort nicht abgerufen"}</Text> */}
       <Text>{weather ? `Temperatur: ${weather.temperature}°C` : "Keine Wetterdaten verfügbar"}</Text>
     </View>
